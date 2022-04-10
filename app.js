d567d258f7d265af6cdba40b23f1dc63efe12c06
Asadullah Baig <61774598+Asadullah-Baig@users.noreply.github.com>
2022-04-10 20:17:47 +0500

    swagger docs added

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//Swagger
+const SwaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocment = YAML.load("./swagger.yaml");
+
 //connect DB
 const connectDB = require("./db/connect");
 
@@ -36,8 +41,10 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerDocment));
 
 // routes
 app.use("/api/v1/auth", authRouter);
